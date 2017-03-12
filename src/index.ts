9a972e1352d7ad0614dd607a5286488ba3129933
John Freeman <john.freeman.opensource@gmail.com>
2017-03-12 17:10:29 +0000

    Updated external API (#99)
    Now includes all the new Kotlin generators.

--- src/index.ts
+++ src/index.ts
@@ -25,16 +25,22 @@ export {
   HandlerParameter,
   HttpMethod,
   HttpMethodHandler,
+  HttpStatus,
   IntegerType,
+  Mapping,
   PathElement,
   PathParameter,
   PathParameterReference,
+  PathScope,
   Property,
   Response,
   RootPathScope,
+  SimpleType,
   Specification,
   StaticPathElement,
-  StringType
+  StringType,
+  SubPathScope,
+  Type
 } from './restrulz/model';
 export {SwaggerGenerator} from './swagger/generator';
 export {
@@ -48,3 +54,48 @@ export {
   SwaggerSpecification,
   SwaggerResponse
 } from './swagger/schema';
+export {
+  AbstractClassKt,
+  AnnotationKt,
+  AnnotationParameterKt,
+  ArgumentKt,
+  BodyContentKt,
+  BodyKt,
+  CatchBlockKt,
+  ClassKt,
+  ClassMemberKt,
+  CompanionObjectKt,
+  CompanionObjectMemberKt,
+  ConstructorPropertyKt,
+  DynamicLineKt,
+  ExtendsKt,
+  ExtensionFunctionKt,
+  ExtendsOrImplementsKt,
+  FileKt,
+  FileMemberKt,
+  FunctionKt,
+  FunctionSignatureKt,
+  IfBlockKt,
+  ImplementsKt,
+  InitBlockKt,
+  InterfaceKt,
+  LineKt,
+  ObjectKt,
+  ParameterKt,
+  PrimaryConstructorKt,
+  PropertyKt,
+  TextKt,
+  TryBlockKt,
+  TypeSignatureKt,
+  VisibilityKt,
+  WhenCaseKt,
+  WhenKt,
+  WhileBlockKt
+} from './kotlin/lang';
+export {KotlinSerializer} from './kotlin/serializer';
+export {KotlinGenerator} from './kotlin/generator';
+export {KotlinModelGenerator} from './kotlin/model-generator';
+export {KotlinValidatorGenerator} from './kotlin/jvm/validator-generator';
+export {KotlinJsonWriterGenerator} from './kotlin/jvm/json-writer-generator';
+export {KotlinJsonReaderGenerator} from './kotlin/jvm/json-reader-generator';
+export {KotlinSpringMvcGenerator} from './kotlin/jvm/spring-mvc-generator';
